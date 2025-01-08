cb5f0d14745ee6cd2b74d737b79dcf431e3ad3b9
seaking110 <seaking110@naver.com>
2025-01-08 16:48:38 +0900

    Lv.3 람다식과 스트림을 이용한 큰 결과만 조회 메서드 구현 완료 및 조회 방식 변경

--- src/main/java/com/example/calculator3/App.java
+++ src/main/java/com/example/calculator3/App.java
@@ -6,11 +6,11 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("계산기 프로그램을 시작합니다!");
-        ArithmeticCalculator <Double> cal = new ArithmeticCalculator();
+        ArithmeticCalculator<Double> cal = new ArithmeticCalculator();
         // exit를 받을 때까지 무한루프
         while (true) {
-            double num1 =0; // 첫번째 입력값
-            double num2 =0; // 두번째 입력값
+            double num1 = 0; // 첫번째 입력값
+            double num2 = 0; // 두번째 입력값
 
             //입력 부분
             System.out.println("첫번째 실수를 입력하세요!");
@@ -34,8 +34,8 @@ public class App {
 
             // 계산 중 에러 발생 시 이슈 발생 여부 체크 변수의 값 변경 및 오류 메세지 출력
             try {
-                result = cal.calculate(num1,num2,operatorType);
-            } catch (CalculatorException e){
+                result = cal.calculate(num1, num2, operatorType);
+            } catch (CalculatorException e) {
                 issue = true;
                 System.out.println(e.getMessage());
             }
@@ -43,33 +43,58 @@ public class App {
             if (!issue) {
                 cal.setSaveResult(result);
                 System.out.println("결과: " + result);
-            }
 
-            // 전체 결과 출력
-            System.out.print("현재까지 연산한 결과는 :");
-            for (double d : cal.getsaveResult()) {
-                System.out.print(d+" ");
             }
-            System.out.println();
-
-            // 맨 앞의 연산 결과 삭제
-            System.out.println("연산한 결과들의 맨 앞을 삭제하시겠습니까? (yes 입력시 삭제)");
-            String delete = sc.next();
-            // yes 라면 Calculator의 삭제 메소드를 호출하여 객체의 필드에 간접 접근하여 컬렉션 필드 값 수정
-            if (delete.equals("yes")) {
-                cal.removeResult();
-                System.out.print("현재까지 연산한 결과는 :");
-                for (double d : cal.getsaveResult()) {
-                    System.out.print(d+" ");
-                }
+            boolean flag = true;
+            while (flag) {
                 System.out.println();
-            }
-            // 무한 루프를 빠져나가는 조건문
-            System.out.println("더 계산하시겠습니까? (exit 입력시 종료)");
-            String s = sc.next();
-            if (s.equals("exit")) {
-                System.out.println("계산기 프로그램을 종료합니다!");
-                break;
+                System.out.println("전체 연산 조회 : 1");
+                System.out.println("첫번째 연산 결과 삭제 : 2");
+                System.out.println("계산기 프로그램 종료 : 3 (또는 exit)");
+                System.out.println("입력 값보다 큰 값 조회 : 4");
+                System.out.println("다시 계산 하시려면 그외의 값을 입력해주세요!!!");
+
+                String s = sc.next();
+                switch (s) {
+                    case "1": {
+                        System.out.print("현재까지 연산한 결과는 :");
+                        for (double d : cal.getsaveResult()) {
+                            System.out.print(d + " ");
+                        }
+                        System.out.println();
+                        break;
+                    }
+                    case "2": {
+                        cal.removeResult();
+                        System.out.print("첫번째 연산을 삭제한 뒤 연산한 결과는 : ");
+                        for (double d : cal.getsaveResult()) {
+                            System.out.print(d + " ");
+                        }
+                        System.out.println();
+                        break;
+                    }
+                    case "3": case "exit": {
+                        System.out.println("계산기 프로그램을 종료합니다!");
+                        System.exit(0);
+                    }
+                    case "4": {
+                        System.out.println("기준이 될 값을 입력해주세요!!");
+                        double min = sc.nextDouble();
+                        if (cal.largeResult(min).size() == 0){
+                            System.out.println("큰 값이 존재하지 않습니다!");
+                        }
+                        else {
+                            System.out.print(min+"보다 큰 값은 : ");
+                            for (double d : cal.getLargeResult()){
+                                System.out.print(d+" ");
+                            }
+                            System.out.println();
+                        }
+                        break;
+                    }
+                    default: flag = false;
+                }
+
             }
         }
     }
--- src/main/java/com/example/calculator3/ArithmeticCalculator.java
+++ src/main/java/com/example/calculator3/ArithmeticCalculator.java
@@ -7,16 +7,26 @@ import java.util.List;
 public class ArithmeticCalculator <T extends Number>{
     // 필드
     //계산 결과 값을 저장할 컬렉션 필드
-    private List<T> saveResult = new ArrayList<>();
+    private List<Double> saveResult = new ArrayList<>();
+
+    private List<Double> largeResult = new ArrayList<>();
     private T t;
     //메서드
     // 컬렉션에 값을 추가하는 Setter 메서드
-    public void setSaveResult(T num) {
+    public void setSaveResult(double num) {
         this.saveResult.add(num);
     }
+
+    public void setlargeResult(double num) {
+        this.largeResult.add(num);
+    }
     // 컬렉션 필드에 접근하여 값을 가져오는 Getter 메서드
-    public List<T> getsaveResult() {
+    public List<Double> getsaveResult() {
         return saveResult;
+    }
+
+    public List<Double> getLargeResult() {
+        return largeResult;
     }
 
     public void set(T t) {
@@ -42,7 +52,12 @@ public class ArithmeticCalculator <T extends Number>{
             }
         };
     }
-
+    // 입력 받은 값보다 큰 결과물만 출력
+    public List<Double> largeResult(double d) {
+        largeResult.clear();
+        saveResult.stream().filter(f-> f > d).forEach(f -> largeResult.add(f));
+        return getLargeResult();
+    }
     // 컬렉션 필드의 맨 앞 값 삭제 메서드
     public void removeResult() {
         saveResult.remove(0);
